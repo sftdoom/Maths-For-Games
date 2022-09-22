dc0b5ec8d46ce411a247eac3f23bb8195b8de7e9
Cameron Danitschek <cameron.danitschek@gmail.com>
2022-09-22 09:25:16 -0700

    Add initial math project

---
+++ MathFormulas/MathsFormulas.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFormulas
+{
+    public static class MathsFormulas
+    {
+        //Problem a - Basic Quadratic 
+        public static float BasicQuadratic(float x)
+        {
+            //x^2 + 2x - 7
+            return x * x + 2 * x - 7;
+        }
+        //Problem B - Quadratic Curve
+        public struct QuadraticRoots
+        {
+            public float RootA;
+            public float RootB;
+            public bool hasRoots;
+        }
+        public static QuadraticRoots GetQuadraticRoots(float a, float b, float c)
+        {
+            //calc numerator
+            float numA = -b + MathF.Sqrt(b * b - 4 * a * c);
+            float numB = -b - MathF.Sqrt(b * b - 4 * a * c);
+
+            //calc denominator 
+            float den = 2 * a;
+
+            //create roots objects
+            QuadraticRoots roots = new QuadraticRoots();
+            roots.RootA = numA / den;
+            roots.RootB = numB / den;
+            if ((b * b - 4 * a * c)<0)
+            {
+                roots.hasRoots = false;
+            }
+            else
+            {
+                roots.hasRoots = true;
+            }
+
+            //return
+            return roots;
+
+            
+        }
+        //Problem C - Linear Blend (lerp)
+        public static float LinearBlend(float s, float e, float t)
+        {
+            float numA = e - s;
+            float numB = t * numA;
+            float blendedVar = s + numB;
+
+            return blendedVar;
+        }
+
+        public static float DistanceBetweenTwoPoints(float x1, float x2, float y1, float y2)
+        {
+            return MathF.Sqrt((x2-x1)*(x2- x1) + (y2-y1)*(y2 - y1));
+        }
+
+        
+    }
+}
---
+++ MathFormulas/Program.cs
@@ -0,0 +1,22 @@
+﻿using MathFormulas;
+using System;
+using System.Diagnostics;
+
+namespace AIE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float quad = MathsFormulas.BasicQuadratic(5.0f);
+            Debug.Assert(quad == 28, "Invalid result for BasicQuadrativ");
+            Debug.Assert(MathF.Abs(quad - 28) < 0.0001, "Valid");
+
+            MathsFormulas.QuadraticRoots roots= MathsFormulas.GetQuadraticRoots(1,-5,4);
+
+            float lerp = MathsFormulas.LinearBlend(4, 6, 0.5f);
+
+            return;
+        }
+    }
+}
